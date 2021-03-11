459c11e45ff40ab99778f4318163ea102a0c591d
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-03-11 17:01:06 -0500

    add self-hosted tiles

--- js/mapSources.js
+++ js/mapSources.js
@@ -3,7 +3,10 @@ const sources = {
     type: "vector",
     url: "https://tiles.dvrpc.org/data/dvrpc-municipal.json",
   },
-  // add more sources here
+  lts_tool: {
+    type: "vector",
+    url: "http://157.230.82.238:8080/data/test-case.json",
+  },
 };
 
 export default sources;
