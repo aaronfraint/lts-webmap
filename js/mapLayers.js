da74e86e1c13341d05f22cfce2ef9d3218189b7f
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-03-11 17:01:19 -0500

    add tile data

--- js/mapLayers.js
+++ js/mapLayers.js
@@ -20,7 +20,70 @@ const layers = {
       "line-color": "#4a5c64",
     },
   },
-  // add more layers here
+  links: {
+    id: "links",
+    type: "line",
+    source: "lts_tool",
+    "source-layer": "links",
+    paint: {
+      "line-width": 1,
+      "line-color": "#FFFFFF",
+      "line-dasharray": [2, 4],
+    },
+  },
+  islandsHighlighted: {
+    id: "islands-highlighted",
+    type: "line",
+    source: "lts_tool",
+    "source-layer": "islands",
+    paint: {
+      "line-width": 20,
+      "line-color": "yellow",
+      "line-opacity": 0.8,
+    },
+    filter: ["in", "island_id", ""],
+  },
+  islands: {
+    id: "islands",
+    type: "line",
+    source: "lts_tool",
+    "source-layer": "islands",
+    paint: {
+      "line-color": ["get", "rgb"],
+      "line-width": 10,
+    },
+  },
+  islandsSelected: {
+    id: "islands-selected",
+    type: "line",
+    source: "lts_tool",
+    "source-layer": "islands",
+    paint: {
+      "line-color": ["get", "rgb"],
+      "line-width": 8,
+    },
+    filter: ["in", "island_id", ""],
+  },
+  linksHighlighted: {
+    id: "links-highlighted",
+    type: "line",
+    source: "lts_tool",
+    "source-layer": "links",
+    paint: {
+      "line-width": 10,
+      "line-color": "cyan",
+      "line-opacity": 0.5,
+    },
+    filter: ["in", "gid", ""],
+  },
 };
 
-export default layers;
+const paint_props = {
+  islands: {
+    id: "islands",
+    prop: "line-width",
+    style: ["interpolate", ["exponential", 0.5], ["zoom"], 10, 0.75, 17, 4],
+  },
+};
+
+export { layers, paint_props };
