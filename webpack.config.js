1557735c7b6f0e2d8513355104ce2274e3a0c276
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-03-16 16:04:31 -0400

    add css plugin

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  mode: "production",
+
+  entry: {
+    main: path.resolve(__dirname, "./webmap/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "testing 213123123123",
+      template: path.resolve(__dirname, "./webmap/template.html"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
+  },
+
+  module: {
+    rules: [
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+};
